c2ab964bf8c7ff4ba8f481fce06915f6c2747ba7
WinUP <l.gholoi@outlook.com>
2018-06-15 11:41:24 -0400

    Bug fix of git (1/2).

--- src/producers/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './StructuredDataPick.producer';
-export * from './ValueConvert.producer';
-export * from './DataPicker.producer';
-export * from './Empty.producer';
-export * from './Wrap.producer';
